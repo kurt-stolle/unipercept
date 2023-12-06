979d4aa38f2be35b7d8b04b359d3c662120bbdd7
Kurt Stolle <kurt-stolle@users.noreply.github.com>
2023-12-06 16:58:58 +0100

    Bump version

--- sources/unipercept/__init__.py
+++ sources/unipercept/__init__.py
@@ -1,7 +1,7 @@
 """UniPercept: A Python package for the analysis of perceptual data and the training of deep neural networks in PyTorch."""
 # from __future__ import annotations
 
-__version__ = "5.0.1"
+__version__ = "5.1.0"
 
 from . import config, data, engine, evaluators, log, model, nn, render, utils
 from ._api_config import *

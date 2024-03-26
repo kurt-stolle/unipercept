4bd215d8b210728ab7b08336e7ff459aae0ae7cb
Kurt Stolle <kurt-stolle@users.noreply.github.com>
2024-03-26 12:06:37 +0100

    CLI command

---
+++ sources/unipercept/cli/ros2.py
@@ -0,0 +1,34 @@
+"""
+Run UniPercept in training/inference as a ROS2 node. Not for production use.
+"""
+import unipercept as up
+
+import rclpy
+import argparse
+from ._command import command
+from ._config import add_config_args
+
+
+@command(help="trian a model", description=__doc__)
+@command.with_config
+def ros2(p: argparse.ArgumentParser):
+    p.add_argument(
+        "--size", type=int, help="Size of the input images in pixels (smallest side)"
+    )
+    p.add_argument(
+        "--weights",
+        "-w",
+        type=str,
+        help="path to load model weights from (overrides any state recovered by the config)",
+    )
+    
+    return main
+
+
+def main(args):
+    model = up.create_model(args.config, weights=args.weights)    
+    pass
+
+
+if __name__ == "__main__":
+    command.root("ros2")
